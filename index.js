cd7f00bb5b376eccdc4b69f2e7ab74a7b2c99f00
Saifulnovel <sinovel07@gmail.com>
2022-10-25 17:56:46 +0600

    index.file-complete

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require('cors')
+const courses = require('./courses.json')
+
+
+app.use(cors())
 
 app.get("/", (req, res) => {
   res.send("Assignmment api server running");
 });
+app.get('/courses', (req, res) => {
+  res.send(courses)
+})
 
 app.listen(port, () => {
   console.log("Learning Platform file reloading");
